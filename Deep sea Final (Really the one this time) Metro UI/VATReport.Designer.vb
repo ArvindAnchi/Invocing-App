1f2d011788bc1a683d483cc9e1bc06a80cecf278
Arvind Anchi <arvindanchi@hotmail.com>
2020-11-23 17:37:20 +0400

    Added option to save VAT Report as excel file

--- Deep sea Final (Really the one this time) Metro UI/VATReport.Designer.vb
+++ Deep sea Final (Really the one this time) Metro UI/VATReport.Designer.vb
@@ -19,9 +19,9 @@ Partial Class VATReport
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.VatDGV = New System.Windows.Forms.DataGridView()
         Me.AmmountDetailsLbl = New System.Windows.Forms.ToolStripStatusLabel()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
@@ -29,6 +29,7 @@ Partial Class VATReport
         Me.VATSDateDTP = New System.Windows.Forms.DateTimePicker()
         Me.IEDateLBL = New System.Windows.Forms.Label()
         Me.ISDateLBL = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.VatDGV, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -38,30 +39,33 @@ Partial Class VATReport
         Me.VatDGV.AllowUserToAddRows = False
         Me.VatDGV.AllowUserToDeleteRows = False
         Me.VatDGV.AllowUserToResizeRows = False
-        DataGridViewCellStyle4.BackColor = System.Drawing.Color.Azure
-        Me.VatDGV.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle4
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.Azure
+        Me.VatDGV.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.VatDGV.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.VatDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.VatDGV.BackgroundColor = System.Drawing.Color.Gainsboro
         Me.VatDGV.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.VatDGV.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleVertical
         Me.VatDGV.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
-        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
-        DataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle5.Font = New System.Drawing.Font("Tahoma", 8.25!)
-        DataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.Window
-        DataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.Window
-        DataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.VatDGV.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle5
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.VatDGV.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
         Me.VatDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window
-        DataGridViewCellStyle6.Font = New System.Drawing.Font("Tahoma", 8.25!)
-        DataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer))
-        DataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Window
-        DataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer))
-        DataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
-        Me.VatDGV.DefaultCellStyle = DataGridViewCellStyle6
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Tahoma", 8.25!)
+        DataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer))
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(38, Byte), Integer))
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.VatDGV.DefaultCellStyle = DataGridViewCellStyle3
         Me.VatDGV.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically
         Me.VatDGV.EnableHeadersVisualStyles = False
         Me.VatDGV.GridColor = System.Drawing.Color.FromArgb(CType(CType(200, Byte), Integer), CType(CType(200, Byte), Integer), CType(CType(200, Byte), Integer))
@@ -70,7 +74,7 @@ Partial Class VATReport
         Me.VatDGV.ReadOnly = True
         Me.VatDGV.RowHeadersVisible = False
         Me.VatDGV.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.VatDGV.Size = New System.Drawing.Size(607, 164)
+        Me.VatDGV.Size = New System.Drawing.Size(1059, 416)
         Me.VatDGV.TabIndex = 7
         '
         'AmmountDetailsLbl
@@ -82,9 +86,9 @@ Partial Class VATReport
         'StatusStrip1
         '
         Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AmmountDetailsLbl})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 202)
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 500)
         Me.StatusStrip1.Name = "StatusStrip1"
-        Me.StatusStrip1.Size = New System.Drawing.Size(607, 22)
+        Me.StatusStrip1.Size = New System.Drawing.Size(1059, 22)
         Me.StatusStrip1.TabIndex = 8
         Me.StatusStrip1.Text = "StatusStrip1"
         '
@@ -120,11 +124,24 @@ Partial Class VATReport
         Me.ISDateLBL.TabIndex = 10
         Me.ISDateLBL.Text = "Start Date:"
         '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(0, 457)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(1059, 40)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Save excel"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'VATReport
         '
+        Me.AcceptButton = Me.Button1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(607, 224)
+        Me.ClientSize = New System.Drawing.Size(1059, 522)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.VATEDateDTP)
         Me.Controls.Add(Me.VATSDateDTP)
         Me.Controls.Add(Me.IEDateLBL)
@@ -132,8 +149,6 @@ Partial Class VATReport
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.VatDGV)
         Me.IconOptions.ShowIcon = False
-        Me.MaximizeBox = False
-        Me.MaximumSize = New System.Drawing.Size(607, 254)
         Me.MinimizeBox = False
         Me.MinimumSize = New System.Drawing.Size(607, 254)
         Me.Name = "VATReport"
@@ -155,4 +170,5 @@ Partial Class VATReport
     Friend WithEvents VATSDateDTP As DateTimePicker
     Friend WithEvents IEDateLBL As Label
     Friend WithEvents ISDateLBL As Label
+    Friend WithEvents Button1 As Button
 End Class
--- Deep sea Final (Really the one this time) Metro UI/VATReport.vb
+++ Deep sea Final (Really the one this time) Metro UI/VATReport.vb
@@ -1,6 +1,34 @@
-﻿Public Class VATReport
-    Private Sub VATReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports ExcelInterop = Microsoft.Office.Interop.Excel
 
+Public Class VATReport
+    Private Sub VATReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        VATSDateDTP.Value = New DateTime(Today.Year, Today.Month - 3, 1, 0, 0, 0)
+        VATEDateDTP.Value = New DateTime(Today.Year, Today.Month - 1, Date.DaysInMonth(Today.Year, Today.Month - 1), 0, 0, 0)
+
+        Using dt As DataTable = Main.DBOp.GetVATReport(VATSDateDTP.Value, VATEDateDTP.Value)
+            Dim dataView As DataView = dt.DefaultView
+
+            VatDGV.DataSource = dt
+
+            With VatDGV
+                .Columns(0).FillWeight = 14.29
+                .Columns(1).FillWeight = 48.57
+                .Columns(2).FillWeight = 8.29
+                .Columns(3).FillWeight = 15.43
+                .Columns(4).FillWeight = 7.71
+                .Columns(5).FillWeight = 20
+                .Columns(6).FillWeight = 8.14
+                .Columns(7).FillWeight = 4.86
+                .Columns(8).FillWeight = 10.29
+            End With
+
+            AmmountDetailsLbl.Text = "Net Total: " & dt.Compute("SUM([Net total])", dataView.RowFilter) &
+                " VAT: " & dt.Compute("SUM([VAT])", dataView.RowFilter) &
+                " Grand total: " & dt.Compute("SUM([Grand total])", dataView.RowFilter)
+        End Using
+    End Sub
+
+    Private Sub VATSDateDTP_ValueChanged(sender As Object, e As EventArgs) Handles VATSDateDTP.ValueChanged, VATEDateDTP.ValueChanged
         Using dt As DataTable = Main.DBOp.GetVATReport(VATSDateDTP.Value, VATEDateDTP.Value)
             Dim dataView As DataView = dt.DefaultView
 
@@ -12,15 +40,76 @@
         End Using
     End Sub
 
-    Private Sub VATSDateDTP_ValueChanged(sender As Object, e As EventArgs) Handles VATSDateDTP.ValueChanged
-        Using dt As DataTable = Main.DBOp.GetVATReport(VATSDateDTP.Value, VATEDateDTP.Value)
-            Dim dataView As DataView = dt.DefaultView
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim excel As ExcelInterop._Application = New ExcelInterop.Application()
+        Dim workbook As ExcelInterop._Workbook = excel.Workbooks.Add(Type.Missing)
+        Dim worksheet As ExcelInterop._Worksheet = Nothing
 
-            VatDGV.DataSource = dt
+        Try
 
-            AmmountDetailsLbl.Text = "Net Total: " & dt.Compute("SUM([Net total])", dataView.RowFilter) &
-                " VAT: " & dt.Compute("SUM([VAT])", dataView.RowFilter) &
-                " Grand total: " & dt.Compute("SUM([Grand total])", dataView.RowFilter)
-        End Using
+            worksheet = workbook.ActiveSheet
+            worksheet.Name = "VAT Report"
+
+            Dim CellRowIndex As Integer = 1
+            Dim cellColumnIndex As Integer = 1
+
+            For J As Integer = 0 To VatDGV.ColumnCount - 1
+                worksheet.Cells(CellRowIndex, cellColumnIndex) = VatDGV.Columns(J).HeaderText
+                cellColumnIndex += 1
+            Next
+            cellColumnIndex = 1
+            CellRowIndex += 1
+
+            For i As Integer = 0 To VatDGV.Rows.Count - 1
+                For J As Integer = 0 To VatDGV.ColumnCount - 1
+                    worksheet.Cells(CellRowIndex, cellColumnIndex) = VatDGV.Rows(i).Cells(J).Value
+                    cellColumnIndex += 1
+                Next
+                cellColumnIndex = 1
+                CellRowIndex += 1
+            Next
+
+            worksheet.Rows("1:1").Font.FontStyle = "Bold"
+            worksheet.Rows("1:1").HorizontalAlignment = ExcelInterop.Constants.xlLeft
+            worksheet.Columns("A:I").VerticalAlignment = ExcelInterop.Constants.xlCenter
+            worksheet.Columns("A:I").HorizontalAlignment = ExcelInterop.Constants.xlLeft
+            worksheet.Columns("D").NumberFormat = "0"
+            worksheet.Columns("G").NumberFormat = "0.00"
+            worksheet.Columns("H").NumberFormat = "0.00"
+            worksheet.Columns("I").NumberFormat = "0.00"
+
+            worksheet.Cells.Columns.AutoFit()
+            worksheet.Cells.Select()
+            worksheet.Cells.EntireColumn.AutoFit()
+            worksheet.Cells(1, 1).Select()
+
+            Dim SaveDialog As New SaveFileDialog()
+            SaveDialog.Filter = "Excel Files(*.xlsx)|*.xlsx|All files (*.*)|*.*"
+            SaveDialog.FilterIndex = 1
+
+            If SaveDialog.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+                workbook.SaveAs(SaveDialog.FileName)
+                Process.Start(SaveDialog.FileName)
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            excel.Quit()
+
+            releaseObject(excel)
+            releaseObject(workbook)
+            releaseObject(worksheet)
+        End Try
+    End Sub
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
     End Sub
 End Class
